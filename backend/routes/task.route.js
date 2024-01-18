a7a9872e481abc1d9bd2ab1aa09db1c6675fcbf4
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-01-18 13:27:58 +0530

    updated task route and created feedback & blacklist model

--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const { auth } = require("../middlewares/auth.middleware");
 const { TaskModel } = require("../models/task.model");
 
-const router = express.Router();
+const taskRoute = express.Router();
 
-router.get("/", async (req, res) => {
+taskRoute.use(auth);
+
+taskRoute.get("/", async (req, res) => {
   try {
     const tasks = await TaskModel.find();
     if (tasks.length === 0) throw "Please create some tasks first";
@@ -13,15 +16,38 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/add", async (req, res) => {
+taskRoute.post("/add", async (req, res) => {
   const payload = req.body;
   try {
     const task = new TaskModel(payload);
     await task.save();
-    res.status(201).json({ message: "New task has been created" });
+    res.status(201).json({ msg: "New task has been created" });
   } catch (error) {
     res.status(400).json({ error });
   }
 });
 
-module.exports = router;
+taskRoute.patch("/update/:taskID", async (req, res) => {
+  const payload = req.body;
+  try {
+    if (payload.userID !== req.params.taskID) throw "Unauthorized: You're not authorized to change this task.";
+    await TaskModel.findByIdAndUpdate(req.params.taskID, payload);
+    res.status(201).json({ msg: "The task has been updated successfully" })
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+taskRoute.delete("/update/:taskID", async (req, res) => {
+  try {
+    if (req.body.userID !== req.params.taskID) throw "Unauthorized: You're not authorized to delete this task.";
+    await TaskModel.findByIdAndUpdate(req.params.taskID, payload);
+    res.status(201).json({ msg: "The task has been updated successfully" })
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+module.exports = {
+  taskRoute,
+};
