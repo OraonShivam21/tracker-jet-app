e73deca6c6f13500c1e7c19d598a56c48228edeb
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-01-20 20:58:47 +0530

    added all task section

--- frontend/dashboard/dashboard.js
+++ frontend/dashboard/dashboard.js
@@ -48,18 +48,25 @@ function menuItemClicked(menuItem, dashboardContentID) {
     dashboardContentNavs[i].classList.remove("active");
   }
   document.getElementById(dashboardContentID).classList.add("active");
+  fetchAndShowMyDayTask();
 }
 
 const addMyDayTaskBtn = document.getElementById("add-my-day-task-btn");
 const addMyDayTask = document.getElementById("addMyDayTask");
+const addAllMyTaskBtn = document.getElementById("add-all-my-task-btn");
+const addAllMyTask = document.getElementById("addAllMyTask");
 
 addMyDayTask.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") addMyDayNewTask();
+  if (e.key === "Enter") addMyDayNewTask(1);
 });
 
-function addMyDayNewTask() {
+addAllMyTask.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") addMyDayNewTask(2);
+});
+
+function addMyDayNewTask(num) {
   const taskAddURL = "http://localhost:3000/tasks/add";
-  const title = addMyDayTask.value;
+  const title = num === 1 ? addMyDayTask.value : addAllMyTask.value;
   fetch(taskAddURL, {
     method: "POST",
     headers: {
@@ -78,6 +85,7 @@ function addMyDayNewTask() {
 }
 
 const myDayTasksContent = document.getElementById("my-day-tasks-content");
+const allMyTasksContent = document.getElementById("all-my-tasks-content");
 
 function fetchAndShowMyDayTask() {
   const getTasksURL = "http://localhost:3000/tasks";
@@ -109,8 +117,10 @@ function fetchAndShowMyDayTask() {
 
 function renderMyDayTasks(tasks) {
   myDayTasksContent.innerHTML = null;
+  allMyTasksContent.innerHTML = null;
   tasks.forEach((task) => {
     myDayTasksContent.appendChild(createMyDayTaskCard(task));
+    allMyTasksContent.appendChild(createMyDayTaskCard(task));
   });
 
   const taskCards = document.getElementsByClassName("task-card");
