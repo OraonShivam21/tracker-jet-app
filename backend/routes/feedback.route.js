b8fc2c28a6507eecdd8726b30a7314908baa6008
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-01-21 16:34:34 +0530

    updated feedback model and route

--- backend/routes/feedback.route.js
+++ backend/routes/feedback.route.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const {FeedbackModel} = require("../models/feedback.model");
+const { FeedbackModel } = require("../models/feedback.model");
 const { auth } = require("../middlewares/auth.middleware");
 
 const feedbackRoute = express.Router();
 
 feedbackRoute.get("/", async (req, res) => {
   try {
-    console.log("Hello")
     const feedbacks = await FeedbackModel.find().sort({ updatedAt: -1 });
     if (feedbacks.length === 0) {
-      res.json({msg:"No feedback found"})
-      console.log("No feedback")
-    };
+      res.json({ msg: "No feedback found" });
+      console.log("No feedback");
+    }
     res.status(200).json({ feedbacks });
   } catch (error) {
     res.status(400).json({ error });
